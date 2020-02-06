2b7b3f16d051a4f9bb9f502859f5b984e42223e0
daleonv <daleonv@utn.edu.ec>
2020-02-06 08:30:19 -0500

    Login funcional con contraseñas encriptadas SHA1

--- registroficaEJB/ejbModule/registrofica/model/manager/ManagerPersona.java
+++ registroficaEJB/ejbModule/registrofica/model/manager/ManagerPersona.java
@@ -1,5 +1,9 @@
 package registrofica.model.manager;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -32,45 +36,58 @@ public class ManagerPersona {
 	}
 
 	public reg_persona iniciarSesion(reg_persona us) {
+		// encriptado
+		String pass = us.getPassword();
+		byte[] newPassword = null;
+		try {
+			newPassword = MessageDigest.getInstance("SHA").digest(pass.getBytes("UTF-8"));
+		} catch (NoSuchAlgorithmException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		String encriptado = Base64.getEncoder().encodeToString(newPassword);
+		// codigo
 		reg_persona persona = null;
 		String consulta;
 		try {
 			consulta = "FROM reg_persona u WHERE u.cedula= ?1 and u.password= ?2";
 			Query query = em.createQuery(consulta);
 			query.setParameter(1, us.getCedula());
-			query.setParameter(2, us.getPassword());
-			
+			query.setParameter(2, encriptado);
+
 			List<reg_persona> lista = query.getResultList();
 			if (!lista.isEmpty()) {
 				persona = lista.get(0);
 			}
 		} catch (Exception e) {
 			throw e;
-		} 
+		}
 		return persona;
 	}
-	
+
 	public List<reg_persona> findAllPersona() {
 		return em.createNamedQuery("reg_persona.findAll").getResultList();
 	}
-	
+
 	public reg_persona findPersonabyId(String cedula) {
 		return em.find(reg_persona.class, cedula);
 	}
-	
+
 	public void insertarPersona(reg_persona persona, int codigoCarrera) throws Exception {
 		aca_carrera carrera;
 		carrera = managerCarrera.findCarrerabyId(codigoCarrera);
 		persona.setAcaCarrera(carrera);
 		em.persist(persona);
 	}
-	
+
 	public void eliminarPersona(String cedula) throws Exception {
 		reg_persona per = findPersonabyId(cedula);
 		if (per != null)
 			em.remove(per);
 	}
-	
+
 	public void modificarPersona(reg_persona persona, int codigoCarrera) throws Exception {
 		reg_persona per = findPersonabyId(persona.getCedula());
 		aca_carrera carrera;
