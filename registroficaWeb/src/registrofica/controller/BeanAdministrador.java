ddecbfaff11b859723085f27434aacbfdfe7ff3a
adrunf <adrunf@localhost>
2020-02-11 16:36:34 -0500

    Página - CRUD de Administrador y botón de regresar al index

--- registroficaWeb/src/registrofica/controller/BeanAdministrador.java
+++ registroficaWeb/src/registrofica/controller/BeanAdministrador.java
@@ -6,9 +6,11 @@ import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 import registrofica.model.entities.adm_administrador;
+import registrofica.model.entities.reg_estado;
 import registrofica.model.manager.ManagerAdministrador;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Named
 @SessionScoped
@@ -21,10 +23,14 @@ public class BeanAdministrador implements Serializable {
 	@EJB
 	private ManagerAdministrador managerAdministrador;
 	private adm_administrador administrador;
+	private adm_administrador administradorSeleccionado;
+	private List<adm_administrador> lista_administradores;
+	private String username;
 
 	@PostConstruct
 	public void init() {
 		administrador = new adm_administrador();
+		lista_administradores = managerAdministrador.findAllAdministrador();
 	}
 
 	public adm_administrador getAdministrador() {
@@ -35,6 +41,45 @@ public class BeanAdministrador implements Serializable {
 		this.administrador = administrador;
 	}
 
+	public void actionListenerInsertarAdministrador() {
+		try {
+			managerAdministrador.insertarAdministrador(administrador);
+			lista_administradores = managerAdministrador.findAllAdministrador();
+			administrador = new adm_administrador();
+			JSFUtil.crearMensajeInfo("Datos de administrador ingresados");
+		} catch (Exception e) {
+			e.getMessage();
+			JSFUtil.crearMensajeError(e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+	public void actionListenerEliminarEstado(String user) {
+		try {
+			managerAdministrador.eliminarAdministrador(user);
+			lista_administradores = managerAdministrador.findAllAdministrador();
+			administrador = new adm_administrador();
+			JSFUtil.crearMensajeInfo("Datos eliminados");
+		} catch (Exception e) {
+			e.getMessage();
+			JSFUtil.crearMensajeError(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	public void actionListenerAdministradorSeleccionado(adm_administrador adm) {
+		administradorSeleccionado = adm;
+	}
+
+	public void actualizarAdministrador() {
+		try {
+			managerAdministrador.modificarAdministrador(administradorSeleccionado, username);
+			JSFUtil.crearMensajeInfo("Usuario modificado");
+		} catch (Exception e) {
+			JSFUtil.crearMensajeError(e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
 	public String iniciarSesion() {
 		adm_administrador pe;
@@ -42,15 +87,47 @@ public class BeanAdministrador implements Serializable {
 		try {
 			pe = managerAdministrador.iniciarSesion(administrador);
 			if (pe != null) {
-				//aquí se debe cambiar la ruta 
+				// aquí se debe cambiar la ruta
 				redireccion = "menuAdmin";
 				JSFUtil.crearMensajeInfo("Acceso correcto");
-			}else {
+			} else {
 				JSFUtil.crearMensajeError("Datas incorrectos");
 			}
 		} catch (Exception e) {
 		}
 		return redireccion;
+	}
+
+	public ManagerAdministrador getManagerAdministrador() {
+		return managerAdministrador;
+	}
+
+	public void setManagerAdministrador(ManagerAdministrador managerAdministrador) {
+		this.managerAdministrador = managerAdministrador;
+	}
+
+	public adm_administrador getAdministradorSeleccionado() {
+		return administradorSeleccionado;
+	}
+
+	public void setAdministradorSeleccionado(adm_administrador administradorSeleccionado) {
+		this.administradorSeleccionado = administradorSeleccionado;
+	}
+
+	public List<adm_administrador> getLista_administradores() {
+		return lista_administradores;
+	}
+
+	public void setLista_administradores(List<adm_administrador> lista_administradores) {
+		this.lista_administradores = lista_administradores;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 }
