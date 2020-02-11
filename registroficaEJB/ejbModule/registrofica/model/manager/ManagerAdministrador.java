7b2aedc0858892dad1ab1407ff001fb2e505b535
adrunf <adrunf@localhost>
2020-02-11 15:37:10 -0500

    Ingreso métodos CRUD de administrador

--- registroficaEJB/ejbModule/registrofica/model/manager/ManagerAdministrador.java
+++ registroficaEJB/ejbModule/registrofica/model/manager/ManagerAdministrador.java
@@ -8,7 +8,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import registrofica.model.entities.aca_carrera;
 import registrofica.model.entities.adm_administrador;
+import registrofica.model.entities.reg_persona;
 
 /**
  * Session Bean implementation class ManagerPersona
@@ -45,4 +47,26 @@ public class ManagerAdministrador {
 		return administrador;
 	}
 
+	public List<adm_administrador> findAllAdministrador() {
+		return em.createNamedQuery("adm_administrador.findAll").getResultList();
+	}
+	public adm_administrador findAdministradorbyId(String user) {
+		return em.find(adm_administrador.class, user);
+	}
+	public void insertarAdministrador(adm_administrador admin) throws Exception {
+		em.persist(admin);
+	}
+	public void eliminarAdministrador(String user) throws Exception {
+		adm_administrador admin = findAdministradorbyId(user);
+		if (admin != null)
+			em.remove(admin);
+	}
+	public void modificarAdministrador(adm_administrador admin, String user) throws Exception {
+		adm_administrador adm = findAdministradorbyId(user);
+		if (adm == null)
+			throw new Exception("No existe esta usuario");
+		adm.setUsername(admin.getUsername());
+		adm.setPassword(admin.getPassword());
+		em.merge(adm);
+	}
 }
